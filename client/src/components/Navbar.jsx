510588c724cdb41f58de893ff3dbf22e62ac7561
@Masum0895 <2221793@iub.edu.bd>
2025-12-05 17:46:02 +0600

    Home page in progress

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -12,7 +12,7 @@ const Navbar = () => {
   return (
     <nav className="flex items-center justify-between px-6 md:px-16 lg:px-24 xl:px-32 py-4 border-b border-gray-300 bg-white relative transition-all">
 
-            <NavLink to='/'>
+            <NavLink to='/' onClick={()=> setOpen(false)}>
                 <img className='h-9' src={assets.logo} alt='logo' />
             </NavLink>
 
@@ -28,14 +28,24 @@ const Navbar = () => {
                     <img src={assets.search_icon} alt='search' className='w-4 h-4'/>
                 </div>
 
-                <div className="relative cursor-pointer">
+                <div onClick={()=> navigate("/cart")} className="relative cursor-pointer">
                     <img src={assets.nav_cart_icon} alt='cart' className='w-6 optacity-80'/>
                     <button className="absolute -top-2 -right-3 text-xs text-white bg-primary-500 w-[18px] h-[18px] rounded-full">3</button>
                 </div>
 
-                <button className="cursor-pointer px-8 py-2 bg-primary hover:bg-primary-dull transition text-white rounded-full">
+                {!user ?(
+                    <button onClick={()=> setShowUserLogin(true)} className="cursor-pointer px-8 py-2 bg-primary hover:bg-primary-dull transition text-white rounded-full">
                     Login
                 </button>
+                ) : (
+                    <div className='relative group'>
+                        <img src={assets.profile_icon} className='w-10' alt='avatar' />
+                        <ul className=' hidden group-hover:block absolute top-10 right-0 bg-white shadow border border-gray-200 py-2.5 w-30 rounded-md text-sm z-40'>
+                            <li onClick={()=>navifate("my-orders")} className='p-1.5 pl-3 hover:bg-primary/10 cursor-pointer'>My Orders</li>
+                            <li onClick={logout}className='p-1.5 pl-3 hover:bg-primary/10 cursor-pointer'>Logout</li>
+                        </ul>
+                    </div>
+                )}
             </div>
 
             <button onClick={() => open ? setOpen(false) : setOpen(true)} aria-label="Menu" className="sm:hidden">
@@ -44,6 +54,7 @@ const Navbar = () => {
             </button>
 
             {/* Mobile Menu */}
+            { open && (
             <div className={`${open ? 'flex' : 'hidden'} absolute top-[60px] left-0 w-full bg-white shadow-md py-4 flex-col items-start gap-2 px-5 text-sm md:hidden`}>
                 <NavLink to='/' onClick={()=> setOpen(false)}>Home</NavLink>
                 <NavLink to='/products' onClick={()=> setOpen(false)}>All Product</NavLink>
@@ -60,13 +71,13 @@ const Navbar = () => {
                     Login
                 </button>
                 ):(
-                    <button className="cursor-pointer px-6 py-2 mt-2 bg-primary-500 hover:bg-primary-dull-600 transition text-white rounded-full text-sm">
+                    <button onClick={logout} className="cursor-pointer px-6 py-2 mt-2 bg-primary-500 hover:bg-primary-dull-600 transition text-white rounded-full text-sm">
                     Logout
-                </button>
-                )
-                }
+                </button>              
+                )}
                 
             </div>
+            )}
 
         </nav>
   )
