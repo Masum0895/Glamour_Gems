373364fa5d181d2a62bb20521f841ea416b74519
@Masum0895 <2221793@iub.edu.bd>
2025-12-20 01:06:23 +0600

    CartItem Count Completed

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -4,7 +4,7 @@ import {assets} from '../assets/assets.js'
 import { useAppContext } from '../context/AppContext'
 const Navbar = () => {
     const [open, setOpen] = React.useState(false)
-    const {user, setUser, setShowUserLogin, navigate, setSearchQuery, searchQuery} = useAppContext();
+    const {user, setUser, setShowUserLogin, navigate, setSearchQuery, searchQuery, getCartCount} = useAppContext();
     const logout = async ()=>{
         setUser(null);
         Navigate('/')
@@ -37,7 +37,7 @@ const Navbar = () => {
 
                 <div onClick={()=> navigate("/cart")} className="relative cursor-pointer">
                     <img src={assets.nav_cart_icon} alt='cart' className='w-6 optacity-80'/>
-                    <button className="absolute -top-2 -right-3 text-xs text-white bg-primary-500 w-[18px] h-[18px] rounded-full">3</button>
+                    <button className="absolute -top-2 -right-3 text-xs text-white bg-primary w-[18px] h-[18px] rounded-full">{getCartCount()}</button>
                 </div>
 
                 {!user ?(
@@ -55,10 +55,19 @@ const Navbar = () => {
                 )}
             </div>
 
-            <button onClick={() => open ? setOpen(false) : setOpen(true)} aria-label="Menu" className="sm:hidden">
+            <div className='flex items-center gap-6 sm:hidden'>
+                <div onClick={()=> navigate("/cart")} className="relative cursor-pointer">
+                    <img src={assets.nav_cart_icon} alt='cart' className='w-6 optacity-80'/>
+                    <button className="absolute -top-2 -right-3 text-xs text-white bg-primary w-[18px] h-[18px] rounded-full">{getCartCount()}</button>
+                </div>
+
+                <button onClick={() => open ? setOpen(false) : setOpen(true)} aria-label="Menu" className="">
                 {/* Menu Icon SVG */}
-                 <img src={assets.menu_icon} alt='menu'/>
-            </button>
+                    <img src={assets.menu_icon} alt='menu'/>
+                </button>
+            </div>
+
+            
 
             {/* Mobile Menu */}
             { open && (
